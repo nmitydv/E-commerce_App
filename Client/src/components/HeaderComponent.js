44484045b0f6463c5a3a1b219cb002c51131726e
namit <namity.bca2022@ssism.org>
2023-11-24 14:48:12 +0530

    module Half completed

--- Client/src/components/HeaderComponent.js
+++ Client/src/components/HeaderComponent.js
@@ -1,4 +1,47 @@
-const HeaderComponent = () =>{
-    return <p>This is header Component</p>
- }
- export default HeaderComponent;
+import { Navbar, Nav, Container, NavDropdown, InputGroup, Badge, Form ,DropdownButton , Dropdown  , Button} from 'react-bootstrap';
+const HeaderComponent = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <InputGroup>
+                        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </DropdownButton>
+                        <Form.Control type="text" placeholder="Search" />
+                        <Button variant="warning">Warning</Button>
+                        </InputGroup>
+                        </Nav>
+                        <Nav>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#pricing1">
+                            <Badge pill bg="danger">
+                                2
+                            </Badge>
+                            CART
+                        </Nav.Link>
+                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    );
+}
+export default HeaderComponent;
