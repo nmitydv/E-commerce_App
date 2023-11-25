8a12e7c785f3a253dba4a96d8387cec8276430b2
namit <namity.bca2022@ssism.org>
2023-11-25 19:21:09 +0530

    Footer completed successfully

--- Client/src/components/FooterComponent.js
+++ Client/src/components/FooterComponent.js
@@ -1,4 +1,16 @@
-const FooterComponent = () =>{
-   return  <p>this is footer component</p>
+import { Container, Row, Col } from 'react-bootstrap';
+import '../index.js';
+const FooterComponent = () => {
+
+   return (
+      <footer className='mt-5'>
+         <Container fluid>
+            <Row>
+               <Col className='bg-dark text-white text-center py-5'>Copyright &copy; Best Online Shop MARTPIN</Col>
+            </Row>
+         </Container>
+      </footer>
+   )
+
 }
 export default FooterComponent;
