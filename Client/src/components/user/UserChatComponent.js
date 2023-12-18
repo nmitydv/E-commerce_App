0335562144c5df2dd95820daa9a8ef76c5467eb7
namit <namity.bca2022@ssism.org>
2023-12-18 14:03:34 +0530

    bug fix

--- Client/src/components/user/UserChatComponent.js
+++ Client/src/components/user/UserChatComponent.js
@@ -7,12 +7,12 @@ const UserChatComponent = () => {
                 <i className="bi bi-chat-dots comment"></i>
                 <i className="bi bi-x-circle close"></i>
             </label>
-            {/* <div className="chat-wrapper">
+            <div className="chat-wrapper">
                 <div className="chat-header">
                     <h6>Let's Chat - Online</h6>
                 </div>
             </div>
-            <div className="chat-form">
+            {/* <div className="chat-form">
                 <div className="chat-msg">
                     <p>Chat history</p>
                 </div>
