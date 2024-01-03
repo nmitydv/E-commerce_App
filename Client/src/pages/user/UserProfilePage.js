fe7ee9c0f9853ad3cd632343a0be1e5b0e4c274f
Namit Yadav <yadavnamit605@gmail.com>
2024-01-03 12:01:18 +0530

    bug fixed

--- Client/src/pages/user/UserProfilePage.js
+++ Client/src/pages/user/UserProfilePage.js
@@ -119,7 +119,7 @@ const UserProfilePage = () => {
                 onChange={onChange}
               />
               <Form.Control.Feedback type="invalid">
-                Please anter a valid password
+                Please enter a valid password
               </Form.Control.Feedback>
               <Form.Text className="text-muted">
                 Password should have at least 6 characters
